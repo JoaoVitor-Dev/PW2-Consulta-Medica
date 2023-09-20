0d4dd78b5367871b1785dc4cb5d0604c5a3739cb
JoaoVitor-dev <joaovitorreal@outlook.com>
2023-09-20 20:17:33 -0300

    iniciado crud de medico

--- src/main/java/com/ifto/mapeamento/controller/MedicoController.java
+++ src/main/java/com/ifto/mapeamento/controller/MedicoController.java
@@ -1,5 +1,7 @@
 package com.ifto.mapeamento.controller;
 
+import com.ifto.mapeamento.model.entity.Medico;
+import com.ifto.mapeamento.model.entity.Paciente;
 import com.ifto.mapeamento.model.repository.MedicoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,6 +19,12 @@ import org.springframework.web.servlet.ModelAndView;
 public class MedicoController {
     @Autowired
     MedicoRepository repository;
+
+    @GetMapping("/form")
+    public String form(Medico medico){
+        return "/medico/form";
+    }
+
 
     @GetMapping("/list")
     public ModelAndView listarMedico(ModelMap model) {
@@ -29,5 +38,21 @@ public class MedicoController {
         return new ModelAndView("medico/consulta", model);
     }
 
+    @PostMapping("/save")
+    public ModelAndView salvarPaciente(Medico medico){
+        repository.save(medico);
+        return new ModelAndView("redirect:/medico/list");
+    }
 
+    @GetMapping("/removeMedico/{id}")
+    public ModelAndView remove(@PathVariable("id") Long id){
+        repository.remove(id);
+        return new ModelAndView("redirect:/medico/list");
+    }
+
+    @GetMapping("/editMedico/{id}")
+    public ModelAndView editPaciente(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("medico", repository.medico(id));
+        return new ModelAndView("/medico/form", model);
+    }
 }
