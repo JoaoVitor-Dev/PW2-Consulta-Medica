6c0b9aa232738e3d49c5af06b5077dc53c3ab98c
JoaoVitor-dev <joaovitorreal@outlook.com>
2023-12-06 23:23:28 -0300

    Alterado regras de segurança de usuários

--- src/main/java/com/ifto/mapeamento/SecurityConfiguration.java
+++ src/main/java/com/ifto/mapeamento/SecurityConfiguration.java
@@ -28,13 +28,14 @@ public class SecurityConfiguration {
                         customizer ->
                                 customizer
                                         .requestMatchers("/paciente/form").permitAll()
-                                        .requestMatchers("/paciente/list").hasAnyRole("ADMIN")
                                         .requestMatchers("/medico/form").hasAnyRole("ADMIN")
-                                        .requestMatchers("/medico/list").hasAnyRole("ADMIN")
+                                        .requestMatchers("/medico/*").hasAnyRole("ADMIN")
                                         .requestMatchers("/paciente/consulta").permitAll()
                                         .requestMatchers("/consulta/form").hasAnyRole("ADMIN")
-                                        //.requestMatchers(HttpMethod.POST,"/paciente/save").permitAll()
-                                        //.requestMatchers(HttpMethod.POST,"/medico/save").hasAnyRole("ADMIN")
+                                        .requestMatchers(HttpMethod.POST,"/paciente/save").permitAll()
+                                        .requestMatchers(HttpMethod.POST,"/consulta/save").hasAnyRole("ADMIN")
+                                        .requestMatchers(HttpMethod.POST,"/consulta/update").hasAnyRole("ADMIN")
+                                        .requestMatchers("/consulta/remove/{id}").hasAnyRole("ADMIN")
                                         .anyRequest() //define que a configuração é válida para qualquer requisição.
                                         .authenticated() //define que o usuário precisa estar autenticado.
 
